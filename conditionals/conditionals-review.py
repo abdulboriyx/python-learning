a4c7dadda0e9c999c510d7852f18e54c26b21c5a
MacBook <zerozae@MacBook-Air.local>
2025-01-11 14:05:51 +0500

    Conditionals: review

---
+++ conditionals/conditionals-review.py
@@ -0,0 +1,49 @@
+# x = int(input('What is x? '))
+# y = int(input('What is y? '))
+
+# if x < y: 
+#       print('X is less than Y')
+
+
+# x = int(input('What is X? '))
+# y = int(input('What is Y? '))
+
+# if x < y:
+#       print('X is less than Y')
+# elif x > y: 
+#       print('X is greater than Y')
+# else:
+#       print('X is equal to Y')
+
+
+# def main():
+#       x = int(input('What is X? '))
+#       if is_odd(x):
+#             print('Odd')
+#       else:
+#             print('Even')
+
+# def is_odd(y):
+#       if y % 2 != 0:
+#             return True
+#       else: 
+#             return False
+      
+# main()
+
+
+# or
+
+
+name_club = input('What is your club? ')
+
+if name_club == 'Barcelona' or name_club == 'Atletico Madrid' or name_club == 'Real Madrid':
+      print('La Liga')
+elif name_club == 'Manchester City' or name_club == 'Manchester United' or name_club == 'Liverpool':
+      print('EPL')
+elif name_club == 'PSG' or name_club == 'Lyon' or name_club == 'Marsel' or name_club == 'Lens':
+      print('Ligue 1')
+elif name_club == 'Bayern Munich' or name_club == 'Borussia Dortmund' or name_club == 'Leipzig':
+      print('Bundesliga')
+else:
+      print('Average Ligue')
