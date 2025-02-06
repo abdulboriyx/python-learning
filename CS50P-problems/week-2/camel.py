30af2f1a9a151fdfd679d1481c0e7727dff322ef
MacBook <zerozae@MacBook-Air.local>
2025-02-07 01:14:06 +0900

    Week 2: problems

---
+++ CS50P-problems/week-2/camel.py
@@ -0,0 +1,18 @@
+# # solution
+# def main():
+#       camelcase = input('Get your camel snaked: ')
+#       print((f'Snaked: {get_snaked(camelcase)}'))
+
+# def get_snaked(camel):
+#       snaked = ''
+#       for case in camel:
+#             if (case.isupper()):
+#                   snaked += '_'
+#                   snaked += case.lower()
+#             else:
+#                   snaked += case
+#       return snaked
+
+# main()
+
+
