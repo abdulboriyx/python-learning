1594bb965040ba48b541d309e72e2027564b9dea
MacBook <zerozae@MacBook-Air.local>
2025-03-04 23:59:23 +0900

    Inheritance

---
+++ OOP/inheritance.py
@@ -0,0 +1,27 @@
+class Job:
+      def __init__(self, company, position, salary):
+            self.company = company
+            self.position = position
+            self.salary = salary
+      def recruite(self):
+            return f'{self.company}: We are hiring {self.position} with {self.salary:,}$ salary a year'
+class Google(Job):
+      def __init__(self, company, position, salary, remote):
+            super().__init__(company, position, salary)
+            self.remote = remote
+      def recruite(self):
+            return (f'{super().recruite()}. You can work in {self.remote} regime. ')
+      
+class Meta(Job):
+      def __init__(self, company, position, salary, remote, holiday):
+            super().__init__(company, position, salary)
+            self.remote = remote
+            self.holiday = holiday
+      def recruite(self):
+            return f'{super().recruite()}. You can work {self.remote} regime. You can have {self.holiday} days of annual holiday'
+
+google = Google('Google', 'Manager', 160000, 'hybrid (in-office and remote)')
+meta = Meta('Meta', 'Machine Learning Engineer', 220000, 'hybrid (in-office and remote)', 30)
+
+print(google.recruite())
+print(meta.recruite())
